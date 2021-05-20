f3f005a61beb2d50720c0aaa8e8a21f1aac7825a
Befa222 <72620186+Befa222@users.noreply.github.com>
2021-05-20 19:33:14 +0100

    added some media queries for mobile/tablets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import GitHub from '../images/GitHub-Mark-Light-64px.png';
+import Linkedin from '../images/linkedin-64.ico';
+import Email from '../images/email-64.ico';
 import '../style/home.css';
 
 export default function Home() {
     return (
         <div>
-            <header className='intro'>
+            <header>
                 <div className='neon-sign'>
                     <div id='firstName'>
                         <h1 className='letter-F'>F</h1>
@@ -16,7 +20,7 @@ export default function Home() {
                     </div>
                     <h2 id='lastName'>Lallement</h2>
                 </div>
-                <div className='intro-break'></div>
+                <div className='header-break'></div>
             </header>
             <section className='about-me'>
                 <h2>Welcome</h2>
@@ -26,6 +30,27 @@ export default function Home() {
                 <h3>I hope you'll enjoy the tour!</h3>
                 <div className='about-me-break'></div>
             </section>
+            <section className='projects'>
+                <h1>PROJECTS</h1>
+                <div className='projects-container'>
+                    <div className='project1'></div>
+                    <div className='project2'></div>
+                    <div className='project3'></div>
+                    <div className='project4'></div>
+                </div>
+
+            </section>
+            <footer>
+                <div className='footer-break-top'></div>
+                <h1>LINKS/CONTACT</h1>
+                <div className='links-container'>
+                    <img onClick={() => window.open('https://github.com/Befa222', '_blank')} src={GitHub} alt='Github-icon' />
+                    <img onClick={() => window.open('https://www.linkedin.com/in/fabien-lallement-b10386204/', '_blank')} src={Linkedin} alt='Linkedin-icon' />
+                    <Link to='./Contact'><img src={Email} alt='email-icon' /></Link>
+                </div>
+                <div className='footer-break-bottom'></div>
+                <p>'D2S./2021'</p>
+            </footer>
         </div>
     )
 }
